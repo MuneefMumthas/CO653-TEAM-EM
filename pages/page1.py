618dc31cf804e9c8861bd0095d49b8c9fb8d9ae9
MuneefMumthas <22206529@bucks.ac.uk>
2025-03-21 20:54:57 +0000

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -5,6 +5,7 @@ import base64
 
 st.title("Welcome to CO653 - Loan-EM")
 
+st.markdown("---")
 st.header("Project Overview")
 st.write(
     "We are developing this system as part of our coursework for CO653, where we are required to build a system using either a neural network or fuzzy logic. "
@@ -15,12 +16,15 @@ st.header("Team Members")
 st.write("- **Enkh-Amgalan Enkhbayar (22135347)**")
 st.write("- **Muneef Ahamed Mohamed Mumthas (22206529)**")
 
+st.markdown("---")
 st.header("Dataset")
 st.write(
     "For our dataset, we found one from a hackathon, which we are utilising to train and evaluate our models. "
     "While our primary goal is to complete the coursework assignment, we are also using the hackathon as an opportunity to review our model's score on the leaderboard,"
     "essentially hitting two birds with one stone."
 )
+
+st.markdown("---")
 st.header("About the Hackathon")
 st.write(
     "[Analytics Vidhya Loan Prediction Challenge](https://www.analyticsvidhya.com/datahack/contest/practice-problem-loan-prediction-iii/). "
@@ -29,6 +33,7 @@ st.write(
     "Evaluation is based on **accuracy**, and top models will be featured on the leaderboard."
 )
 
+st.markdown("---")
 st.header("Data Dictionary")
 
 st.subheader("Train file: CSV containing the customers for whom loan eligibility is known as 'Loan_Status'")
