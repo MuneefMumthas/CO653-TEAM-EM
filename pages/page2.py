e8c3d48e85a84c97b04ba56eb93581f3f32d33fc
MuneefMumthas <22206529@bucks.ac.uk>
2025-03-28 02:04:36 +0000

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -18,27 +18,19 @@ def main():
     # Create a dropdown for users to input new data
     gender = st.selectbox("Gender", ['Male', 'Female'])
     married = st.selectbox("Married", ['Yes', 'No'])
+    dependants = st.selectbox("Dependants", ['1', '2', '3+'])
     education = st.selectbox("Education", ['Graduate', 'Not Graduate'])
     self_employed = st.selectbox("Self Employed", ['Yes', 'No'])
-    property_area = st.selectbox("Property Area", ['Urban', 'Rural', 'Semiurban'])
-    credit_history = st.selectbox("Credit History", ['0', '1'])  # Assume '0' means no and '1' means yes
-    applicant_income = st.number_input("Applicant Income", min_value=0)
-    coapplicant_income = st.number_input("Coapplicant Income", min_value=0)
-    loan_amount = st.number_input("Loan Amount", min_value=0)
-    loan_amount_term = st.number_input("Loan Amount Term", min_value=0)
+    
+
     
     # Prepare the input data as a dictionary
     input_data = {
         'Gender': gender,
         'Married': married,
+        'Dependents': dependants,
         'Education': education,
         'Self_Employed': self_employed,
-        'Property_Area': property_area,
-        'Credit_History': credit_history,
-        'ApplicantIncome': applicant_income,
-        'CoapplicantIncome': coapplicant_income,
-        'LoanAmount': loan_amount,
-        'Loan_Amount_Term': loan_amount_term
     }
 
 
