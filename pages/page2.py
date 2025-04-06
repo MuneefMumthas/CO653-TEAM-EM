9eb1307fcc2a03ecf0d74f2daa07a7aa751d6b82
MuneefMumthas <22206529@bucks.ac.uk>
2025-04-06 13:39:28 +0100

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -7,6 +7,13 @@ import joblib
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
 
+
+#Loading the saved objects
+model = tf.keras.models.load_model("pkl/best_model.h5")
+mestimate_encoder_ = joblib.load("pkl/mestimate_encoder.pkl")
+minmax_scaler = joblib.load("pkl/minmax_scaler.pkl")
+label_encoder = joblib.load("pkl/label_encoder.pkl")
+
 st.title("Neural Network 🧠")
 
 st.markdown("---")
@@ -15,23 +22,22 @@ st.markdown("---")
 def main():
     st.title("Loan Approval Prediction")
 
-    # Create a dropdown for users to input new data
-    gender = st.selectbox("Gender", ['Male', 'Female'])
-    married = st.selectbox("Married", ['Yes', 'No'])
-    dependants = st.selectbox("Dependants", ['1', '2', '3+'])
-    education = st.selectbox("Education", ['Graduate', 'Not Graduate'])
-    self_employed = st.selectbox("Self Employed", ['Yes', 'No'])
-    
+    with st.form(key="loan_form"):
+        st.subheader("Enter Applicant Details")
 
-    
-    # Prepare the input data as a dictionary
-    input_data = {
-        'Gender': gender,
-        'Married': married,
-        'Dependents': dependants,
-        'Education': education,
-        'Self_Employed': self_employed,
-    }
+        gender = st.selectbox("Gender", ['Select One', 'Male', 'Female'])
+        married = st.selectbox("Married", ['Select One', 'Yes', 'No'])
+        dependents = st.selectbox("Dependents", ['Select One', '1', '2', '3+'])
+        education = st.selectbox("Education", ['Select One', 'Graduate', 'Not Graduate'])
+        self_employed = st.selectbox("Self Employed", ['Select One', 'Yes', 'No'])
+        applicant_income = st.number_input("Applicant Income", min_value=0)
+        coapplicant_income = st.number_input("Coapplicant Income", min_value=0)
+        loan_amount = st.number_input("Loan Amount", min_value=0)
+        loan_term = st.number_input("Loan Amount Term (in days)", min_value=0)
+        credit_history = st.selectbox("Credit History", ['Select One', 1.0, 0.0])
+        property_area = st.selectbox("Property Area", ['Select One', 'Urban', 'Semiurban', 'Rural'])
+
+        submit_btn = st.form_submit_button(label="Test")
 
 
 if __name__ == "__main__":
