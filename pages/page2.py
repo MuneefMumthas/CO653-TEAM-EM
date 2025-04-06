766d852f100de6a78160bfc87aaec70c3ba40b5c
MuneefMumthas <22206529@bucks.ac.uk>
2025-04-06 13:55:57 +0100

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -27,17 +27,17 @@ def main():
 
         gender = st.selectbox("Gender", ['Select', 'Male', 'Female'])
         married = st.selectbox("Married", ['Select', 'Yes', 'No'])
-        dependents = st.selectbox("Dependents", ['Select', '1', '2', '3+'])
+        dependents = st.selectbox("Dependents", ['Select', '0', '1', '2', '3+'])
         education = st.selectbox("Education", ['Select', 'Graduate', 'Not Graduate'])
         self_employed = st.selectbox("Self Employed", ['Select', 'Yes', 'No'])
-        applicant_income = st.number_input("Applicant Income (per year)", min_value=0)
+        applicant_income = st.number_input("Applicant Income", min_value=0)
         coapplicant_income = st.number_input("Coapplicant Income", min_value=0)
-        loan_amount = st.number_input("Loan Amount", min_value=0)
-        loan_term = st.number_input("Loan Amount Term (in days)", min_value=0)
+        loan_amount = st.number_input("Loan Amount (in thousands)", min_value=0)
+        loan_term = st.number_input("Loan Amount Term (in months)", min_value=0)
         credit_history = st.selectbox("Credit History", ['Select', 1.0, 0.0])
         property_area = st.selectbox("Property Area", ['Select', 'Urban', 'Semiurban', 'Rural'])
 
-        submit_btn = st.form_submit_button(label="Test")
+        submit_btn = st.form_submit_button(label="compile into a dataframe")
 
 
 if __name__ == "__main__":
