0769968c5caac2aaa93a2d1ebc377674a2fbfeb2
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-04-08 13:28:57 +0100

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -65,7 +65,7 @@ if submit_btn:
         missing_fields.append("Loan Term")
 
     if missing_fields:
-        st.toast(f"⚠️ Please select the required field(s): {', '.join(missing_fields)}")
+        st.warning(f"⚠️ Please select the required field(s): {', '.join(missing_fields)}")
     else:
         total_income = applicant_income + coapplicant_income
         loan_income_ratio = loan_amount/total_income
