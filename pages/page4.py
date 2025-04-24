9a69cf6c420706895189236da7e7e3ca0c709429
MuneefMumthas <22206529@bucks.ac.uk>
2025-04-24 10:21:29 +0100

    Update page4.py

--- pages/page4.py
+++ pages/page4.py
@@ -14,8 +14,9 @@ git = get_base64_image(git_logo_url)
 
 st.title("Authors")
 # Create two columns
-col1,col2,col3 = st.columns(3)
 col4,col5,col6,col7 = st.columns(4)
+col1,col2,col3 = st.columns(3)
+
 
 with col1:
     st.subheader("Entwan Enkhbayar",anchor=False)
