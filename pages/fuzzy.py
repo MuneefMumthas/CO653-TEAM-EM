cfa5df7f970c62dc4d510d49e2884bd95d5a7b5c
MuneefMumthas <22206529@bucks.ac.uk>
2025-04-24 13:04:38 +0100

    Update fuzzy.py

--- pages/fuzzy.py
+++ pages/fuzzy.py
@@ -5,3 +5,8 @@ import joblib
 import tensorflow as tf
 from st_circular_progress import CircularProgress
 import time
+
+
+st.title("new fuzzy", anchor=False)
+st.markdown("---")
+st.title("Loan Approval Prediction", anchor=False)
