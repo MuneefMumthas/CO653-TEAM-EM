81c524fa267cf10c83d6787c9d30e23f0b07d01b
MuneefMumthas <22206529@bucks.ac.uk>
2025-04-24 10:29:13 +0100

    Update page4.py

--- pages/page4.py
+++ pages/page4.py
@@ -14,16 +14,35 @@ git = get_base64_image(git_logo_url)
 
 st.title("Authors")
 # Create two columns
-col1,col2, = st.columns(2)
+col1,col2,col3 = st.columns(2)
 col4,col5,col6,col7 = st.columns(4)
 
+with col1:
+    st.markdown(f"""
+                <h3 style="text-align: left; margin-bottom: 0;">Entwan Enkhbayar</h3>
+                <style>
+                    img:hover {{
+                        transform: scale(1.1);
+                    }}
+                </style>""", unsafe_allow_html=True)
+
+with col2: 
+    pass
+
+with col3:
+    st.markdown(f"""
+                <h3 style="text-align: left; margin-bottom: 0;">Entwan Enkhbayar</h3>
+                <style>
+                    img:hover {{
+                        transform: scale(1.1);
+                    }}
+                </style>""", unsafe_allow_html=True)
 
 with col4: 
     pass
 with col5:
     st.markdown(
         f"""
-        <h3 style="text-align: left; margin-bottom: 0;">Entwan Enkhbayar</h3>
         <div style="display: flex; justify-content: left; align-items: center; text-align: center; margin-top: 10px;">
             <a href="https://github.com/Enkhamgalan1230" target="_blank" style="text-decoration: none;">
                 <img src="data:image/png;base64,{git}" 
@@ -31,11 +50,7 @@ with col5:
                     style="width: 50px; height: auto; cursor: pointer; transition: transform 0.3s ease-in-out;">
             </a>
         </div>
-        <style>
-            img:hover {{
-                transform: scale(1.1);
-            }}
-        </style>
+        
         """,
         unsafe_allow_html=True
     )
