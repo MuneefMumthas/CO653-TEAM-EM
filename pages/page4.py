c1c22b395c85f0ff478b96d1f38e64739b328ca2
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-04-24 11:01:04 +0100

    linkedin button added

--- pages/page4.py
+++ pages/page4.py
@@ -4,6 +4,7 @@ import time
 import base64
 
 git_logo_url = "assets/github-mark.png"
+linkedin = "assets/linkedin.png"
 def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
@@ -11,8 +12,7 @@ def get_base64_image(image_path):
 # Convert image to Base64
 
 git = get_base64_image(git_logo_url)
-
-st.title("Authors")
+linkedin = get_base64_image(linkedin)
 st.title("Authors")
 
 # Create two side-by-side columns
@@ -21,13 +21,19 @@ col1, col2 = st.columns(2)
 with col1:
     with st.container(border=True):
         st.subheader("Entwan")
+        st.write("Final year AI student.")
         st.markdown(
             f"""
-            <div style="display: flex; justify-content: center; align-items: center; text-align: center; margin-top: 10px;">
+            <div style="display: flex; justify-content: center; align-items: center; gap: 20px; margin-top: 10px;">
                 <a href="https://github.com/Enkhamgalan1230" target="_blank" style="text-decoration: none;">
                     <img src="data:image/png;base64,{git}" 
-                         alt="github" 
-                         style="width: 50px; height: 50px; cursor: pointer; transition: transform 0.3s ease-in-out;">
+                        alt="github" 
+                        style="width: 40px; height: 40px; cursor: pointer; transition: transform 0.3s ease-in-out;">
+                </a>
+                <a href="https://www.linkedin.com/in/entwan/" target="_blank" style="text-decoration: none;">
+                    <img src="data:image/png;base64,{linkedin}" 
+                        alt="linkedin" 
+                        style="width: 40px; height: 40px; cursor: pointer; transition: transform 0.3s ease-in-out;">
                 </a>
             </div>
             """,
@@ -37,13 +43,19 @@ with col1:
 with col2:
     with st.container(border=True):
         st.subheader("Muneef")
+        st.write("Final year AI student.")
         st.markdown(
             f"""
-            <div style="display: flex; justify-content: center; align-items: center; text-align: center; margin-top: 10px;">
+            <div style="display: flex; justify-content: center; align-items: center; gap: 20px; margin-top: 10px;">
                 <a href="https://github.com/MuneefMumthas" target="_blank" style="text-decoration: none;">
                     <img src="data:image/png;base64,{git}" 
-                         alt="github" 
-                         style="width: 50px; height: 50px; cursor: pointer; transition: transform 0.3s ease-in-out;">
+                        alt="github" 
+                        style="width: 40px; height: 40px; cursor: pointer; transition: transform 0.3s ease-in-out;">
+                </a>
+                <a href="https://www.linkedin.com/in/muneefmumthas/?originalSubdomain=uk" target="_blank" style="text-decoration: none;">
+                    <img src="data:image/png;base64,{linkedin}" 
+                        alt="linkedin" 
+                        style="width: 40px; height: 40px; cursor: pointer; transition: transform 0.3s ease-in-out;">
                 </a>
             </div>
             """,
