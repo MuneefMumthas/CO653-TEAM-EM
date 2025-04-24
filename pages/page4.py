be58ba485f532c0b9e9fe58cec1c14a97960975c
Enkhamgalan1230 <115167095+Enkhamgalan1230@users.noreply.github.com>
2025-04-24 10:56:12 +0100

    Update page4.py

--- pages/page4.py
+++ pages/page4.py
@@ -13,48 +13,39 @@ def get_base64_image(image_path):
 git = get_base64_image(git_logo_url)
 
 st.title("Authors")
-# Create two columns
-col1,col2,col3,col4 = st.columns(4)
+st.title("Authors")
 
-cont3 = st.container()
-cont1 = st.container(border=True)
-cont2 = st.container(border=True)
-with cont3:
-    with col1: 
-        pass
+# Create two side-by-side columns
+col1, col2 = st.columns(2)
 
-    with col2:
-        with cont1:
-            st.subheader("Entwan")
-            st.markdown(
-                f"""
-                <div style="display: flex; justify-content: left; align-items: center; text-align: center; margin-top: 10px;">
-                    <a href="https://github.com/Enkhamgalan1230" target="_blank" style="text-decoration: none;">
-                        <img src="data:image/png;base64,{git}" 
-                            alt="github" 
-                            style="width: 50px; height: 50px; cursor: pointer; transition: transform 0.3s ease-in-out;">
-                    </a>
-                </div>
-                
-                """,
-                unsafe_allow_html=True
-            )
+with col1:
+    with st.container(border=True):
+        st.subheader("Entwan")
+        st.markdown(
+            f"""
+            <div style="display: flex; justify-content: center; align-items: center; text-align: center; margin-top: 10px;">
+                <a href="https://github.com/Enkhamgalan1230" target="_blank" style="text-decoration: none;">
+                    <img src="data:image/png;base64,{git}" 
+                         alt="github" 
+                         style="width: 50px; height: 50px; cursor: pointer; transition: transform 0.3s ease-in-out;">
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
-    with col3:
-        with cont2:
-            st.subheader("Muneef")
-            st.markdown(
-                f"""
-                <div style="display: flex; justify-content: right; align-items: center; text-align: center; margin-top: 10px;">
-                    <a href="https://github.com/MuneefMumthas" target="_blank" style="text-decoration: none;">
-                        <img src="data:image/png;base64,{git}" 
-                            alt="github" 
-                            style="width: 50px; height: 50px; cursor: pointer; transition: transform 0.3s ease-in-out;">
-                    </a>
-                </div>
-                """,
-                unsafe_allow_html=True
-            )
-        
-    with col4:
-        pass
+with col2:
+    with st.container(border=True):
+        st.subheader("Muneef")
+        st.markdown(
+            f"""
+            <div style="display: flex; justify-content: center; align-items: center; text-align: center; margin-top: 10px;">
+                <a href="https://github.com/MuneefMumthas" target="_blank" style="text-decoration: none;">
+                    <img src="data:image/png;base64,{git}" 
+                         alt="github" 
+                         style="width: 50px; height: 50px; cursor: pointer; transition: transform 0.3s ease-in-out;">
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
